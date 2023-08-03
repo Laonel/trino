34804de949337363847a6ba06ccceda2b210ae91
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-08-03 11:34:58 +0200

    Fix ConnectorMetadata javadoc formatting
    Apply whatever IntelliJ wants to have applied so that class reformat
    doesn't change the code.
    
    (cherry picked from commit 827a362a93eb838d74a90227ddc2863e0c8d7c43)

--- core/trino-spi/src/main/java/io/trino/spi/connector/ConnectorMetadata.java
+++ core/trino-spi/src/main/java/io/trino/spi/connector/ConnectorMetadata.java
@@ -133,8 +133,7 @@ public interface ConnectorMetadata
     /**
      * Create initial handle for execution of table procedure. The handle will be used through planning process. It will be converted to final
      * handle used for execution via @{link {@link ConnectorMetadata#beginTableExecute}
-     *
-     * <p/>
+     * <p>
      * If connector does not support execution with retries, the method should throw:
      * <pre>
      *     new TrinoException(NOT_SUPPORTED, "This connector does not support query retries")
@@ -605,8 +604,7 @@ public interface ConnectorMetadata
 
     /**
      * Begin the atomic creation of a table with data.
-     *
-     * <p/>
+     * <p>
      * If connector does not support execution with retries, the method should throw:
      * <pre>
      *     new TrinoException(NOT_SUPPORTED, "This connector does not support query retries")
@@ -639,8 +637,7 @@ public interface ConnectorMetadata
 
     /**
      * Begin insert query.
-     *
-     * <p/>
+     * <p>
      * If connector does not support execution with retries, the method should throw:
      * <pre>
      *     new TrinoException(NOT_SUPPORTED, "This connector does not support query retries")
@@ -686,8 +683,7 @@ public interface ConnectorMetadata
 
     /**
      * Begin materialized view query.
-     *
-     * <p/>
+     * <p>
      * If connector does not support execution with retries, the method should throw:
      * <pre>
      *     new TrinoException(NOT_SUPPORTED, "This connector does not support query retries")

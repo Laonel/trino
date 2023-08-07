72f4576e2b673abd215e03f9dbca0beacf806f3c
Piotr Findeisen <piotr.findeisen@gmail.com>
2023-08-07 18:11:16 +0200

    Fix check in HiveMetadata.getView and update its name
    The method is supposed to get a Trino view that's going to be updated.
    Its check for `translateHiveViews` was redundant, the calling code is
    never supposed to operate on a Hive view, regardless of
    `translateHiveViews` flag. (And if it matter, the `translateHiveViews`
    condition would probably be negated.) It was probably not user-facing
    problem as the calling code is likely unreachable when Hive view
    translation is not enabled.
    
    The commit also adds a check that the table object being operated on
    does not represent a Trino Materialized View. The previously checked
    `isPrestoView` returns true for both, but the table is supposed to
    represent ordinary Trino View.
    
    (cherry picked from commit 65cbdcd93fa10f34739c35d72c5fb9a24f1f9c7b)

--- plugin/trino-hive/src/main/java/io/trino/plugin/hive/HiveMetadata.java
+++ plugin/trino-hive/src/main/java/io/trino/plugin/hive/HiveMetadata.java
@@ -1456,7 +1456,7 @@ public class HiveMetadata
     @Override
     public void setViewComment(ConnectorSession session, SchemaTableName viewName, Optional<String> comment)
     {
-        Table view = getView(viewName);
+        Table view = getTrinoView(viewName);
 
         ConnectorViewDefinition definition = toConnectorViewDefinition(session, viewName, Optional.of(view))
                 .orElseThrow(() -> new ViewNotFoundException(viewName));
@@ -1475,7 +1475,7 @@ public class HiveMetadata
     @Override
     public void setViewColumnComment(ConnectorSession session, SchemaTableName viewName, String columnName, Optional<String> comment)
     {
-        Table view = getView(viewName);
+        Table view = getTrinoView(viewName);
 
         ConnectorViewDefinition definition = toConnectorViewDefinition(session, viewName, Optional.of(view))
                 .orElseThrow(() -> new ViewNotFoundException(viewName));
@@ -1499,11 +1499,12 @@ public class HiveMetadata
         hiveMaterializedViewMetadata.setMaterializedViewColumnComment(session, viewName, columnName, comment);
     }
 
-    private Table getView(SchemaTableName viewName)
+    private Table getTrinoView(SchemaTableName viewName)
     {
         Table view = metastore.getTable(viewName.getSchemaName(), viewName.getTableName())
+                .filter(table -> isHiveOrPrestoView(table) && !isTrinoMaterializedView(table))
                 .orElseThrow(() -> new ViewNotFoundException(viewName));
-        if (translateHiveViews && !isPrestoView(view)) {
+        if (!isPrestoView(view)) {
             throw new HiveViewNotSupportedException(viewName);
         }
         return view;
